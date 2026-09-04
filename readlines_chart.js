9ee04cf7b2d8eef687f647cfd9484987397e0e68
sıla <silaozdemir7532@gmail.com>
2026-09-04 09:35:22 +0300

    chore: Remove temporary development scripts

--- readlines_chart.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('frontend/src/components/QueryResults.jsx', 'utf8').split('\n');
-for(let i = 240; i <= 250; i++) {
-    console.log(`${i+1}: ${lines[i]}`);
-}
